296c6bcd176b939568a317eb43507497319837c0
gizemozkahraman <62880946+gizemozkahraman@users.noreply.github.com>
2020-03-30 14:18:00 +0300

    6. videoya baslandı.
    Cssler uyarlancak

---
+++ product/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+
+
+# Create your models here.
+
+class Category(models.Model):
+    STATUS = (
+        ('True', 'Evet'),
+        ('False', 'Hayır'),
+    )
+    title = models.CharField(max_length=30)
+    description = models.CharField(max_length=255)
+    keywords = models.CharField(max_length=255)
+    status = models.CharField(max_length=10, choices=STATUS)
+    image = models.ImageField(blank=True, upload_to='images/')
+    slug = models.SlugField()
+    parent = models.ForeignKey('self', blank=True, null=True, related_name='children', on_delete=models.CASCADE)
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Product(models.Model):
+    STATUS = (
+        ('True', 'Evet'),
+        ('False', 'Hayır'),
+    )
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=30)
+    description = models.CharField(max_length=255)
+    keywords = models.CharField(max_length=255)
+    status = models.CharField(max_length=10, choices=STATUS)
+    image = models.ImageField(blank=True, upload_to='images/')
+    detail = models.TextField()
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
