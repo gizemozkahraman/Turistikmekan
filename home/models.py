8120a440a596d5aff9c21c184d4462487fa009ae
gizemozkahraman <62880946+gizemozkahraman@users.noreply.github.com>
2020-05-16 16:55:45 +0300

    15. videonun yarısı yapıldı.
    adminde alt kategori gösterimi

--- home/models.py
+++ home/models.py
@@ -1,3 +1,70 @@
+from ckeditor_uploader.fields import RichTextUploadingField
 from django.db import models
 
 # Create your models here.
+from django.forms import ModelForm, Textarea, TextInput
+
+
+class Setting(models.Model):
+    STATUS = (
+        ('True', 'Evet'),
+        ('False', 'Hayır'),
+    )
+    title = models.CharField(max_length=150)
+    description = models.CharField(max_length=255)
+    keywords = models.CharField(max_length=255)
+    company = models.CharField(max_length=50)
+    phone = models.CharField(blank=True, max_length=15)
+    address = models.CharField(blank=True, max_length=100)
+    fax = models.CharField(blank=True, max_length=150)
+    email = models.CharField(blank=True, max_length=50)
+    smtpserver = models.CharField(blank=True, max_length=20)
+    smtpemail = models.CharField(blank=True, max_length=20)
+    smtppassword = models.CharField(blank=True, max_length=10)
+    smtpport = models.CharField(blank=True, max_length=5)
+    icon = models.ImageField(blank=True, upload_to='images/')
+    facebook = models.CharField(blank=True, max_length=50)
+    instagram = models.CharField(blank=True, max_length=50)
+    twitter = models.CharField(blank=True, max_length=50)
+    aboutus = RichTextUploadingField(blank=True)
+    referances = RichTextUploadingField(blank=True)
+    status = models.CharField(max_length=10, choices=STATUS)
+    contact = RichTextUploadingField(blank=True)
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+class ContactFormMessage(models.Model):
+    STATUS = (
+        ('New', 'New'),
+        ('Read', 'Read'),
+    )
+    name = models.CharField(blank=True, max_length=20)
+    email = models.CharField(blank=True, max_length=50)
+    subject = models.CharField(blank=True, max_length=50)
+    message = models.CharField(blank=True, max_length=255)
+    status = models.CharField(max_length=10, choices=STATUS, default='New')
+    ip = models.CharField(blank=True, max_length=20)
+    note = models.CharField(blank=True, max_length=100)
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class ContactFormu(ModelForm):
+    class Meta:
+        model = ContactFormMessage
+        fields = ['name', 'email', 'subject', 'message']
+
+    widgets = {
+        'name': TextInput(attrs={'class': 'form-control', 'placeholder': 'Name & Surname'}),
+        'subject': TextInput(attrs={'class': 'form-control', 'placeholder': 'Subject'}),
+        'email': TextInput(attrs={'class': 'form-control', 'placeholder': 'Email'}),
+        'message': Textarea(attrs={'class': 'form-control', 'placeholder': 'Your Message', 'rows': '5'}),
+
+    }
--- home/views.py
+++ home/views.py
@@ -1,9 +1,49 @@
-from django.http import HttpResponse
+from django.contrib import messages
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
-
 # Create your views here.
+from home.models import Setting, ContactFormu, ContactFormMessage
+from product.models import Product
+
+
 def index(request):
-    text = "Hello Django"
-    context = {'text': text}
+    setting = Setting.objects.get(pk=1)
+    sliderdata= Product.objects.all()[:4]
+    context = {'setting': setting,
+               'page': 'home',
+               'sliderdata': sliderdata
+               }
     return render(request, 'index.html', context)
+
+
+def hakkimizda(request):
+    setting = Setting.objects.get(pk=1)
+    context = {'setting': setting}
+    return render(request, 'hakkimizda.html', context)
+
+
+def referanslar(request):
+    setting = Setting.objects.get(pk=1)
+    context = {'setting': setting}
+    return render(request, 'referanslarimiz.html', context)
+
+
+def iletisim(request):
+    if request.method == 'POST':
+        form = ContactFormu(request.POST)
+        if form.is_valid():
+            data = ContactFormMessage()
+            data.name = form.cleaned_data['name']
+            data.email = form.cleaned_data['email']
+            data.subject = form.cleaned_data['subject']
+            data.message = form.cleaned_data['message']
+            data.ip = request.META.get('REMOTE_ADDR')
+            data.save()
+            messages.success(request, "Mesajınız başarıyla gönderilmiştir. Teşekkür Ederiz")
+            return HttpResponseRedirect('/iletisim')
+
+    setting = Setting.objects.get(pk=1)
+    form = ContactFormu()
+    context = {'setting': setting, 'form': form}
+    return render(request, 'iletisim.html', context)
